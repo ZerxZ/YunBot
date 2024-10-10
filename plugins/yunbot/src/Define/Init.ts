424c7e51fb7458958dba8b572022196ce7f83093
Gaoyao Massimo Hu <ghzero97@gmail.com>
2024-10-10 16:20:14 +0100

    chore: Update koishi.yml and remove unused dependencies

--- plugins/yunbot/src/Define/Init.ts
+++ plugins/yunbot/src/Define/Init.ts
@@ -1,209 +1,217 @@
-import {Computed, Context, Dict, Session, User} from "koishi";
-import { bot, DailyData, Game, Today, Yun} from ".";
+import { Computed, Context, Dict, Session, User } from "koishi";
+import { bot, DailyData, Game, Today, Yun } from ".";
 import fs from "fs"
 import { random } from "../Utils";
+import { Core } from "../Core";
 
-declare module 'koishi'{
-	interface Tables{
-		YunSave:UserData; //备份用
-		oldsave:UserData; //旧存档
-	}
+declare module 'koishi' {
+  interface Tables {
+    YunSave: UserData; //备份用
+    oldsave: UserData; //旧存档
+  }
 
-	interface User{
-		userID:string;
-		chara:string;
-		role:string;
-		reset:number;
-		game:Game;
-		daily:DailyData;
-		flags:any;
-	}
+  interface User {
+    userID: string;
+    chara: string;
+    role: string;
+    reset: number;
+    game: Game;
+    daily: DailyData;
+    flags: any;
+  }
 
-	interface Channel{
-		userList:string[];
-		name:string;
-		announce:Computed<number>;
-		helptimer:Computed<number>;
-	}
+  interface Channel {
+    userList: string[];
+    name: string;
+    announce: Computed<number>;
+    helptimer: Computed<number>;
+  }
 
-	namespace Command {
-		interface Config{
-			ignore?: boolean;
-			yunAction?:boolean;
-			notQQ?:boolean;
-			signed?:boolean;
-		}
-	}
+  namespace Command {
+    interface Config {
+      ignore?: boolean;
+      yunAction?: boolean;
+      notQQ?: boolean;
+      signed?: boolean;
+    }
+  }
 }
 
-export interface UserData{
-	id:any;
-	uid:string;
+export interface UserData {
+  id: any;
+  uid: string;
 
-	name:string;
-	chara:string;
-	reset:number;
-	charalist:Dict<Game>;
-	onebot?:string;
-	discord?:string;
-	kook?:string;
+  name: string;
+  chara: string;
+  reset: number;
+  charalist: Dict<Game>;
+  onebot?: string;
+  discord?: string;
+  kook?: string;
+  sandbox?: string;
 }
 
-export function _extend(ctx:Context){
-	ctx.model.extend("YunSave",{
-		id:"string",
-		uid:"string",
+export function _extend(ctx: Context) {
+  ctx.model.extend("YunSave", {
+    id: "string",
+    uid: "string",
 
-		name:"string",
-		chara:"string",
-		reset:"integer",
-		charalist:"json",
-		onebot:"string",
-		discord:"string",
-		kook:"string",
-	}, {
-		foreign:{
-			uid:['user','userID'],
-			chara:['user','chara'],
-		}
-	})
+    name: "string",
+    chara: "string",
+    reset: "integer",
+    charalist: "json",
+    onebot: "string",
+    discord: "string",
+    kook: "string",
+    sandbox: "string",
+  }, {
+    foreign: {
+      uid: ['user', 'userID'],
+      chara: ['user', 'chara'],
+    }
+  })
 
-	ctx.model.extend("oldsave",{
-		id:"unsigned",
-		uid:"string",
+  ctx.model.extend("oldsave", {
+    id: "unsigned",
+    uid: "string",
 
-		name:"string",
-		chara:"string",
-		reset:"integer",
-		charalist:"json",
-	},{
-		autoInc:true,
-	})
+    name: "string",
+    chara: "string",
+    reset: "integer",
+    charalist: "json",
+  }, {
+    autoInc: true,
+  })
 
-	ctx.model.extend("user",{
-		userID:"string",
-		chara:"string",
-		role:"string",
-		reset:"integer",
-		game:"json",
-		daily:"json",
-		flags:"json",
-	},{
-		autoInc:true,
-	})
+  ctx.model.extend("user", {
+    userID: "string",
+    chara: "string",
+    role: "string",
+    reset: "integer",
+    game: "json",
+    daily: "json",
+    flags: "json",
+  }, {
+    autoInc: true,
+  })
 
-	ctx.model.extend("channel",{
-		userList:"list",
-		name:"string",
-		announce:'unsigned',
-		helptimer:'unsigned',
-	},{
-		autoInc:true
-	})
+  ctx.model.extend("channel", {
+    userList: "list",
+    name: "string",
+    announce: 'unsigned',
+    helptimer: 'unsigned',
+  }, {
+    autoInc: true
+  })
 }
 
 export async function save() {
-	await Today.save()
-	Yun.state.stats = Yun.stats
-	await Yun.save()
+  await Today.save()
+  Yun.state.stats = Yun.stats
+  await Yun.save()
 }
 
-export async function initUserFirst( session:Session ) {
-	let name;
-	let uid = session.userId
+export async function initUserFirst(session: Session) {
+  let name;
+  let uid = session.userId
 
-	name = session.author?.nickname;
-	if(!name) name = session.author?.username;
+  name = session.author?.nickname;
+  if (!name) name = session.author?.username;
 
-	name = UserNameGenerator(session.platform, uid, name)
-	uid = UserIdGenerator(session.platform, uid)
+  name = UserNameGenerator(session.platform, uid, name)
+  uid = UserIdGenerator(session.platform, uid)
 
-	return [name, uid];
+  return [name, uid];
 }
 
-export function UserNameGenerator( pf:string, uid:string, name:string){
-	if(!name) name = uid;
-	if(pf=='onebot') return name;
-	else{
-		const code = `#${random(9)}${random(9)}${random(9)}${random(9)}`
-		return name + code
-	}
+export function UserNameGenerator(pf: string, uid: string, name: string) {
+  if (!name) name = uid;
+  if (pf == 'onebot') return name;
+  else {
+    const code = `#${random(9)}${random(9)}${random(9)}${random(9)}`
+    return name + code
+  }
 }
 
-export function UserIdGenerator( pf:string, uid:string){
-	if(pf=='onebot') return 'QQ#'+uid;
-	else{
-		const code = pf+`#${uid.slice(uid.length-9,uid.length)}`
-		return code
-	}
+export function UserIdGenerator(pf: string, uid: string) {
+  if (pf == 'onebot') return 'QQ#' + uid;
+  else {
+    const code = pf + `#${uid.slice(uid.length - 9, uid.length)}`
+    return code
+  }
 }
 
-export async function initUser( session:Session, userId:string, name:string) {
-	const exist = await userExist(userId)
-	if( exist ){
-		return
-	};
-
-	const newId = await makeID()
-	let data:UserData = {
-		id:newId,
-		uid: userId,
-		name: name,
-		reset:0,
-		chara:"",
-		charalist:{}
-	}
-	data[session.platform] = session.userId
-	await bot.db.create("YunSave", data)
+export async function initUser(session: Session, userId: string, name: string) {
+  const exist = await userExist(userId)
+  if (exist) {
+    return
+  };
+  session.app.logger.info("平台", session.platform.split(":")[0])
+  const platform = session.platform.split(":")[0]
+  const newId = await makeID()
+  let data: UserData = {
+    id: newId,
+    uid: userId,
+    name: name,
+    reset: 0,
+    chara: "",
+    charalist: {}
+  }
+  data[platform] = session.userId
+  session.app.logger.info("初始化用户", data)
+  await bot.db.create("YunSave", data)
 }
 
-export async function getUser(uid:string):Promise<User> {
-	const chk = await bot.db.get('user', {userID:uid})
-	return chk[0]
+export async function getUser(uid: string): Promise<User> {
+  const chk = await bot.db.get('user', { userID: uid })
+  return chk[0]
 }
 
-export async function setDaily(uid:string, data:DailyData) {
-	await bot.db.set('user', {userID:uid}, { daily: data })
+export async function setDaily(uid: string, data: DailyData) {
+  await bot.db.set('user', { userID: uid }, { daily: data })
 }
 
-export async function getName(uid:string){
-	const user = await bot.db.get('user', { userID: uid })
-	//console.log(user.length, user[0].name)
-	//console.log(user)
+export async function getName(uid: string) {
+  const user = await bot.db.get('user', { userID: uid })
+  //console.log(user.length, user[0].name)
+  //console.log(user)
 
-	if(user[0]?.game?.nick?.length)
-		return user[0].game.nick;
-	else if(user[0]?.game?.name?.length)
-		return user[0].game.name
-	else
-		return user[0].name
+  if (user[0]?.game?.nick?.length)
+    return user[0].game.nick;
+  else if (user[0]?.game?.name?.length)
+    return user[0].game.name
+  else
+    return user[0].name
 }
 
-export async function getSaves(uid:string):Promise<Dict<Game>>{
-	const chk = await bot.db.get('YunSave', { uid: uid})
-	return chk[0]?.charalist ?? []
+export async function getSaves(uid: string): Promise<Dict<Game>> {
+  const chk = await bot.db.get('YunSave', { uid: uid })
+  return chk[0]?.charalist ?? []
 }
 
-export async function userExist(uid:string) {
-	const chk = await bot.db.get("YunSave", {uid:uid})
-	return (chk.length > 0 )
+export async function userExist(uid: string) {
+  const chk = await bot.db.get("YunSave", { uid: uid })
+  return (chk.length > 0)
 }
 
-export async function makeID(){
-	let chk = await bot.db.get("YunSave", null);
-	let lastId = chk && chk.length > 0 ? parseInt(chk[chk.length -1]?.id ?? 0) : 0
-	const newid =  lastId +1 + random(3)
-	const id = (chk.length > 10000000 ? chk.length.toString() :'0'.repeat(8-(newid.toString().length)) + newid )
+export async function makeID() {
+  let chk = await bot.db.get("YunSave", { id: { $gt: 0 } });
 
-	return id
+  let lastId = chk && chk.length > 0 ? parseInt(chk[chk.length - 1]?.id ?? 0) : 0
+  const newid = lastId + 1 + random(3)
+  const id = (chk.length > 10000000 ? chk.length.toString() : '0'.repeat(8 - (newid.toString().length)) + newid)
+
+  return id
 }
 
-export async function checkData(session:Session<'userID'|'name'|'chara'|'game'>, uid:string) {
+export async function checkData(session: Session<'userID' | 'name' | 'chara' | 'game'>, uid: string) {
 
-	let chk = await bot.db.get("YunSave", null)
-	let charalist:Dict<Game>
-	if(!chk[0]?.charalist){
-		await initUser(session, uid, session.user.name)
-		await setTimeout(() => {}, 500);
-	}
+
+  session.app.logger.info("检查数据")
+  const _uid = UserIdGenerator(session.platform, session.userId)
+  let chk = await session.app.database.get("YunSave", { uid: _uid });
+  if (!chk[0]?.charalist) {
+    await initUser(session, uid, session.user.name)
+    await setTimeout(() => { }, 500);
+  }
 }
--- plugins/yunbot/src/index.ts
+++ plugins/yunbot/src/index.ts
@@ -1,98 +1,97 @@
 import { Context, Schema } from "koishi";
 import { Core } from "./Core";
-import { resolve } from 'path'
-import {} from '@koishijs/plugin-console'
+import { } from '@koishijs/plugin-console'
 import { Today, Yun } from "./Define";
 
 
 export interface Config {
 
-	tokenPrefix?:string;
-	generateToken?:() => string;
+  tokenPrefix?: string;
+  generateToken?: () => string;
 
-	autoBreak?: boolean;
-	autoRest?: boolean;
-	autoWork?: boolean;
-	autoSleep?: boolean;
-	usingTime?: string;
+  autoBreak?: boolean;
+  autoRest?: boolean;
+  autoWork?: boolean;
+  autoSleep?: boolean;
+  usingTime?: string;
 
-	RepeatImg?:number;
-	Laugh?:number;
-	RandomImg?:number;
-	RandomPoke?:number;
-	event?:string;
-	notice?:string;
-	anounce?:string;
-	eventday?:number;
-	bonuslist?:string;
-	shopitems?:shopitems[];
+  RepeatImg?: number;
+  Laugh?: number;
+  RandomImg?: number;
+  RandomPoke?: number;
+  event?: string;
+  notice?: string;
+  anounce?: string;
+  eventday?: number;
+  bonuslist?: string;
+  shopitems?: shopitems[];
 }
 export interface shopitems {
-	name:string;
-	type:string;
-	stock:number;
-	discount:number;
-	value?:number;
+  name: string;
+  type: string;
+  stock: number;
+  discount: number;
+  value?: number;
 }
 
 export const name = 'yunbot'
 
-export const ShopItem:Schema<shopitems> = Schema.object(
-	{
-		name:Schema.string().required().description('商品名称'),
-		type:Schema.string().required().description('种类'),
-		stock:Schema.number().required().description('每日库存'),
-		discount:Schema.number().default(1.10).description('设置折扣')
-	}
+export const ShopItem: Schema<shopitems> = Schema.object(
+  {
+    name: Schema.string().required().description('商品名称'),
+    type: Schema.string().required().description('种类'),
+    stock: Schema.number().required().description('每日库存'),
+    discount: Schema.number().default(1.10).description('设置折扣')
+  }
 )
 
 export const Config: Schema<Config> = Schema.intersect([
 
-Schema.object({
-	autoBreak: Schema.boolean().default(false).description('是否允许不经过指令或事件，自主突破境界。'),
-	autoRest:  Schema.boolean().default(true).description('是否允许不经过指令或事件，自主进入休息状态。'),
-	autoWork:  Schema.boolean().default(false).description('是否允许不经过指令或事件，自主进入修炼状态。'),
-	autoSleep: Schema.boolean().default(false).description('是否允许不经过指令或事件，自主进入睡眠状态。'),
-	usingTime: Schema.string().default('US').description('设置每日刷新时的标准时区。')
-}).description('基本设置'),
+  Schema.object({
+    autoBreak: Schema.boolean().default(false).description('是否允许不经过指令或事件，自主突破境界。'),
+    autoRest: Schema.boolean().default(true).description('是否允许不经过指令或事件，自主进入休息状态。'),
+    autoWork: Schema.boolean().default(false).description('是否允许不经过指令或事件，自主进入修炼状态。'),
+    autoSleep: Schema.boolean().default(false).description('是否允许不经过指令或事件，自主进入睡眠状态。'),
+    usingTime: Schema.string().default('US').description('设置每日刷新时的标准时区。')
+  }).description('基本设置'),
 
 
-Schema.object({
-	generateToken: Schema.function().hidden(),
-}).description('插件设置'),
+  Schema.object({
+    generateToken: Schema.function().hidden(),
+  }).description('插件设置'),
 
-Schema.object({
-	RepeatImg: Schema.number().default(0.2).description('表情包复读的概率'),
-	Laugh: Schema.number().default(0.2).description('对群友哈哈哈气氛组进行反应的概率'),
-	RandomImg: Schema.number().default(0.03).description('随机表情包的触发概率'),
-	RandomPoke: Schema.number().default(0.03).description('随机戳一戳的概率'),
-}).description('概率设置'),
+  Schema.object({
+    RepeatImg: Schema.number().default(0.2).description('表情包复读的概率'),
+    Laugh: Schema.number().default(0.2).description('对群友哈哈哈气氛组进行反应的概率'),
+    RandomImg: Schema.number().default(0.03).description('随机表情包的触发概率'),
+    RandomPoke: Schema.number().default(0.03).description('随机戳一戳的概率'),
+  }).description('概率设置'),
 
-Schema.object({
-	event: Schema.string().default('').description('设置活动名称'),
-	notice: Schema.string().default('').description('设置公告名称'),
-	anounce: Schema.string().default('').description('设置公告内容'),
-	eventday: Schema.number().default(0).description('设置活动持续天数'),
-	bonuslist:Schema.string().default('').description('设置奖励物品列表')
-}).description('活动设置'),
+  Schema.object({
+    event: Schema.string().default('').description('设置活动名称'),
+    notice: Schema.string().default('').description('设置公告名称'),
+    anounce: Schema.string().default('').description('设置公告内容'),
+    eventday: Schema.number().default(0).description('设置活动持续天数'),
+    bonuslist: Schema.string().default('').description('设置奖励物品列表')
+  }).description('活动设置'),
 
-Schema.object({
-	shopitems:Schema.array(ShopItem).description('设置每日商店的商品。'),
-}).description('商店设置')
+  Schema.object({
+    shopitems: Schema.array(ShopItem).description('设置每日商店的商品。'),
+  }).description('商店设置')
 
 ])
 export const inject = {
   required: ['database'],
 }
-export function apply(ctx:Context, config: Config={}){
-//   ctx.inject(['console',"database"], (ctx) => {
-//     // ctx.console.addEntry({
-//     //   dev: resolve(__dirname, '../client/index.ts'),
-//     //   prod: resolve(__dirname, '../dist'),
-//     // })
-//     console.log(ctx)
-	//})
-	Today.baseDir = ctx.baseDir;
-	Yun.baseDir = ctx.baseDir;
-	Core.init(ctx, config)
+export function apply(ctx: Context, config: Config = {}) {
+  //   ctx.inject(['console',"database"], (ctx) => {
+  //     // ctx.console.addEntry({
+  //     //   dev: resolve(__dirname, '../client/index.ts'),
+  //     //   prod: resolve(__dirname, '../dist'),
+  //     // })
+  //     console.log(ctx)
+  //})
+  Today.baseDir = ctx.baseDir;
+  Yun.baseDir = ctx.baseDir;
+  Core.init(ctx, config)
 }
